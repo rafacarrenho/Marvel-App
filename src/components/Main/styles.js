3bad059631adb68b2a98faa7fb5b0c258b229ea4
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-30 17:23:40 -0300

    Fix: Navigate on mobile

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: calc(100vh - 64px - 90px);
+  height: calc(100vh - 64px);
+  padding-bottom: 90px;
   background-color: #e5e5e5;
   overflow-y: scroll;
 `;
