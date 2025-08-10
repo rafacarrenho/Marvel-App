51b24372ed0b57cad34ed261914ef9e37927e34d
Rafael Carrenho <rafael.carrenho@hotmail.com>
2025-08-10 17:27:22 -0300

    fix: marvel request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,13 @@
 import axios from "axios";
-import md5 from "md5";
 
 const TIMEOUT = 10000; // 10 seconds
 const CHARACTERS_LIMIT = 10;
-const timestamp = new Date().getTime().toString();
-const hash = md5(
-  `${timestamp}${process.env.REACT_APP_MARVEL_PRIVATE_KEY}${process.env.REACT_APP_MARVEL_PUBLIC_KEY}`
-);
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   timeout: TIMEOUT,
   params: {
     apikey: process.env.REACT_APP_MARVEL_PUBLIC_KEY!,
-    ts: timestamp,
-    hash: hash,
     limit: CHARACTERS_LIMIT,
   },
 });
